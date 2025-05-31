50f035470bd9146baff3103ed60ad5cdcc27fa08
Emir <sabirov.e.g@edu.mirea.ru>
2025-05-31 19:45:46 +0300

    Изменено название метода, оставшегося от старой реализации

--- src/main/kotlin/security/hashing/HashingService.kt
+++ src/main/kotlin/security/hashing/HashingService.kt
@@ -1,6 +1,6 @@
 package com.example.security.hashing
 
 interface HashingService {
-    fun generateSaltedHash(value : String) : String
+    fun generateHash(value : String) : String
     fun verify(value: String, hash: String) : Boolean
 }
