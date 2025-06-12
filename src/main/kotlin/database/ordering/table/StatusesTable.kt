388e80a8429a0edab0b9383d98fa6ce7fc26013a
Danila Sokolov <sokolov.dans@gmail.com>
2025-06-12 15:14:51 +0300

    Изменен размер ссылки url для фотографий

--- src/main/kotlin/database/ordering/table/StatusesTable.kt
+++ src/main/kotlin/database/ordering/table/StatusesTable.kt
@@ -6,5 +6,5 @@ object StatusesTable : IntIdTable("statuses") {
     val code = varchar("code", 50)
     val name = varchar("name", 50)
     val globalStatus = varchar("global_status", 50)
-    val icon = varchar("icon", 150)
+    val icon = varchar("icon", 200)
 }
